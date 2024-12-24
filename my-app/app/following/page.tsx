0eec8b270d66ddc8259163ea0c831c66aa82f7a5
Zia <ziaualhassain@gmail.com>
2024-12-24 21:11:43 +0530

    added intial

--- my-app/app/following/page.tsx
+++ my-app/app/following/page.tsx
@@ -2,7 +2,7 @@ export default function FollowingPage() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <h1 className="text-3xl font-bold text-gray-900 mb-8">Following</h1>
-      <p className="text-gray-600">Here you'll see posts from people you follow.</p>
+      <p className="text-gray-600">Here you'll see posts from people you follow initial.</p>
     </div>
   )
 }
